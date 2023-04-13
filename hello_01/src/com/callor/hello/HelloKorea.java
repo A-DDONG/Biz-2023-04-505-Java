9183147f5931d1482eebe2135ea85c91690e304c
A-DDONG <ssc5924@gmail.com>
2023-04-13 10:17:27 +0900

    새로운_리파지토리

---
+++ hello_01/src/com/callor/hello/HelloKorea.java
@@ -0,0 +1,12 @@
+package com.callor.hello;
+
+public class HelloKorea {
+	public static void main(String[] args) {
+		System.out.println(33 + 66);
+		System.out.println(33 - 66);
+		System.out.println(33 * 66);
+		System.out.println(33 / 66);
+		System.out.println(33 % 66);
+		
+	}
+}
