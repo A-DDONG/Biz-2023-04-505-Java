5a1914bbe1c5f71b4badf97090cd7d84ef449c8a
A-DDONG <ssc5924@gmail.com>
2023-05-02 21:05:29 +0900

    home

--- Java_011_Class/src/com/callor/classes/ClassH.java
+++ Java_011_Class/src/com/callor/classes/ClassH.java
@@ -9,18 +9,24 @@ public class ClassH {
 
 		int num1 = 0;
 		int num2 = 0;
-		String str;
+		String str1;
+		String str2;
 
 		while (true) {
 			System.out.println("두 개의 정수를 입력하세요");
 			System.out.println("exit를 입력하면 종료합니다");
 			System.out.print("정수1 >> ");
-				num1 = scan.nextInt();
-				if( num1 == 0.2) {
-					break;
+			str1 = scan.nextLine();
+			if(str1.equals("exit")) {
+				break;
 			}
 			System.out.print("정수2 >> ");
-			num2 = scan.nextInt();
+			str2 = scan.nextLine();
+			if(str2.equals("exit")) {
+				break;
+			}
+			num1 = Integer.parseInt(str1);
+			num2 = Integer.parseInt(str2);
 			System.out.printf("%d + %d = %d\n", num1, num2, num1 + num2);
 			System.out.printf("%d - %d = %d\n", num1, num2, num1 - num2);
 			System.out.printf("%d x %d = %d\n", num1, num2, num1 * num2);
