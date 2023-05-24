0352869a0c15be2edc12b0c8903b3cbad2d349bb
A-DDONG <ssc5924@gmail.com>
2023-05-24 16:42:36 +0900

    bank_계좌정보

--- Java_021_bankDB/src/com/callor/bank/exec/DbExecE.java
+++ Java_021_bankDB/src/com/callor/bank/exec/DbExecE.java
@@ -2,6 +2,7 @@ package com.callor.bank.exec;
 
 import java.util.Scanner;
 
+import com.callor.bank.config.BankInfo;
 import com.callor.bank.service.BankService;
 
 public class DbExecE {
@@ -11,14 +12,15 @@ public class DbExecE {
 		BankService bankService = new BankService();
 		
 		while(true) {
-			System.out.println("=".repeat(100));
-			System.out.println("대한은행 고객관리 시스템");
-			System.out.println("=".repeat(100));
+			BankInfo.headerPrint();
 			System.out.println("수행할 업무를 선택하세요");
 			System.out.println("1. 고객추가");
 			System.out.println("2. 고객 정보 수정");
 			System.out.println("3. 고객 정보 삭제");
 			System.out.println("4. 고객 리스트");
+			System.out.println("5. 고객 정보(계좌) 조회");
+			System.out.println("6. 계좌 개설");
+			System.out.println("7. 입금");
 			System.out.println("9. 업무종료");
 			System.out.println("-".repeat(100));
 			System.out.print("업무선택 >> ");
@@ -36,6 +38,9 @@ public class DbExecE {
 			else if(intSelect == 2) bankService.update();
 			else if(intSelect == 3) bankService.delete();
 			else if(intSelect == 4) bankService.printBuyerList();
+			else if(intSelect == 5) bankService.findUserInfo();
+			else if(intSelect == 6) bankService.makeAcount();
+			else if(intSelect == 7) bankService.inputSystem();
 		}
 		System.out.println("업무끝");
 	}
---
+++ Java_021_bankDB/src/com/callor/bank/service/impl/AccServiceV1.java
@@ -0,0 +1,172 @@
+package com.callor.bank.service.impl;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+import com.callor.bank.config.DBConnection;
+import com.callor.bank.config.DBContract;
+import com.callor.bank.models.AccDto;
+import com.callor.bank.service.AccService;
+
+public class AccServiceV1 implements AccService {
+	
+	// DB 에 연결 session 구축하는 도구
+	protected final Connection dbConn;
+
+	public AccServiceV1() {
+		// dbConn 도구를 이미 만들어둔 DBConnection 클래스 코드를
+		// 사용하여 초기화 한다
+		dbConn = DBConnection.getDBConn();
+	}
+
+	/*
+	 * throws ... try...catch... 으로 처리를 해야하는 코드가 있을 경우 직접 처리하지 않고
+	 * 이 method 를 호출한 곳으로 exception 을 대리 처리하도록 하는 조치
+	 */
+	protected AccDto result2Dto(ResultSet result) throws SQLException {
+
+		AccDto accDto = new AccDto();
+		accDto.acNum = result.getString(DBContract.ACC.ACC_NUM);
+		accDto.acDiv = result.getString(DBContract.ACC.ACC_DIV);
+		accDto.acBuId = result.getString(DBContract.ACC.ACC_BUID);
+		// accDto.acBalance = result.getString(DBContract.ACC.ACC_BALANCE);
+
+		// SELECT 된 데이터의 type 이 확실하게 숫자형(특히 정수형)인 경우
+		// getInt() method 를 사용하여 직접 정수형으로 변환 후 받을 수 있ㄷ
+		accDto.acBalance = result.getInt(DBContract.ACC.ACC_BALANCE);
+		return accDto;
+
+	}
+
+	public List<AccDto> db2List(PreparedStatement pStr) throws SQLException {
+
+		ResultSet result = pStr.executeQuery();
+		List<AccDto> accList = new ArrayList<>();
+		while (result.next()) {
+			AccDto accDto = result2Dto(result);
+			accList.add(accDto);
+		}
+		pStr.close();
+		result.close();
+		return accList;
+	}
+
+	@Override
+	public List<AccDto> selectAll() {
+
+		String sql = " SELECT acNum, acDiv,acBuId,acBalance " 
+						+ " FROM tbl_acc " 
+						+ " ORDER BY acNum ";
+
+		try {
+			PreparedStatement pStr = dbConn.prepareStatement(sql);
+			List<AccDto> accList = db2List(pStr);
+			return accList;
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return null;
+	}
+
+	@Override
+	public AccDto findById(String acNum) {
+		String sql = " SELECT acNum, acDiv,acBuId,acBalance " 
+						+ " FROM tbl_acc "
+						+ " WHERE acNum = ? ";
+
+		PreparedStatement pStr;
+		try {
+			pStr = dbConn.prepareStatement(sql);
+			ResultSet result = pStr.executeQuery();
+
+			if (result.next()) {
+				return result2Dto(result);
+			}
+			result.close();
+			pStr.close();
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+		return null;
+	}
+
+	@Override
+	public int insert(AccDto dto) {
+		String sql = " INSERT INTO tbl_acc(acNum, acDiv, acBuId, acBalance) "
+				+ " VALUES(?,?,?,?) ";
+		try {
+			PreparedStatement pStr = dbConn.prepareStatement(sql);
+			pStr.setString(1, dto.acNum);
+			pStr.setString(2, dto.acDiv);
+			pStr.setString(3, dto.acBuId);
+			pStr.setInt(4, dto.acBalance);
+			
+			int result = pStr.executeUpdate();
+			return result;
+			
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		return 0;
+	}
+
+	@Override
+	public int update(AccDto dto) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int delete(String acNum) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public List<AccDto> findByBuId(String acBuId) {
+
+		String sql = " SELECT acNum, acDiv,acBuId,acBalance " 
+				+ " FROM tbl_acc " 
+				+ " WHERE acBuId = ? "
+				+ " ORDER BY acNum ";
+		
+		PreparedStatement pStr;
+		try {
+
+			pStr = dbConn.prepareStatement(sql);
+			pStr.setString(1, acBuId);
+			List<AccDto> accList = db2List(pStr);
+			return accList;
+
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return null;
+
+	}
+	public String maxAcNum(String date) {
+		String sql = " SELECT substr(max(acNum),9) "
+				+ " FROM tbl_acc "
+				+ " WHERE substr(acNum,0,8) = ? ";
+		try {
+			PreparedStatement pStr = dbConn.prepareStatement(sql);
+			pStr.setString(1, date);
+			ResultSet result = pStr.executeQuery();
+			if(result.next()) {
+				return result.getString(1);
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		return "0";
+	}
+
+}
